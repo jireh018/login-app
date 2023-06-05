c7fd81dd77a62292ad56c1de6724e6308f8c0895
jirehTutondele <106825317+jirehTutondele@users.noreply.github.com>
2023-06-05 18:28:46 -0300

    folders and some files created

---
+++ front-end/src/components/FormInput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const formInput = () => {
+  return (
+    <div>
+      Form Input
+    </div>
+  )
+}
+
+export default formInput
---
+++ front-end/src/context/appContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const appContext = () => {
+  return (
+    <div>
+      app Context
+    </div>
+  )
+}
+
+export default appContext
