a12c1292ffa67bba5a17e1a8b59f0a6c668a6f14
jirehTutondele <106825317+jirehTutondele@users.noreply.github.com>
2023-06-06 00:24:22 -0300

    input value validation

--- front-end/src/components/FormInput.js
+++ front-end/src/components/FormInput.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
-const formInput = ({name, type, value, handleChange, labelText, placeholder}) => {
+const formInput = ({name, type, value, handleChange, validateInput, labelText, placeholder}) => {
   return (
     <div>
-      <label for={name} class="block mb-2 text-sm font-medium text-gray-900">{labelText || name}</label>
+      <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900">{labelText || name}</label>
       <input 
         type={type} 
         name={name} 
         id={name} 
-        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " 
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " 
         placeholder={placeholder} 
         required=""
         onChange={handleChange}
+        onBlur={validateInput}
         />
     </div>
   )
--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -18,6 +18,40 @@ const Register = () => {
 
   const handleChange = (e) => {
     setValues({...values, [e.target.name]: e.target.value})
+    validateInput(e)
+  }
+
+  const validateInput = (e) => {
+    const {name, value} = e.target
+    
+    switch (name) {
+      case 'name':
+        if(!value)
+          console.log('Please enter Name!.')
+        break;
+      case 'email':
+        if(!value)
+          console.log('Please enter Email.')
+        break;
+      case 'password':
+        if(!value){
+          console.log('Please enter Password.')
+        }else if(values.confirmPassword && value !== values.confirmPassword){
+          console.log('Password and Confirm Password dont match.')
+        }
+          
+        break;
+      case 'confirmPassword':
+        if(!value){
+          console.log('Please enter Confirm Password.')
+        }
+        else if(values.password && value !== values.password){
+          console.log('Password and Confirm Password dont match.')
+        }
+        break;
+      default:
+        break;
+    }
   }
 
   const toggleMember = () => {
@@ -26,18 +60,18 @@ const Register = () => {
 
   return (
     <div>
-      <section class="bg-gray-50">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-                <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+      <section className="bg-gray-50">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+                <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
                 Login-App 
             </div>
-            <div class="w-full border border-t-[#19838b] bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+            <div className="w-full border border-t-[#19838b] bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                         {values.isMember? 'Login' : 'Register'} 
                     </h1>
-                    <form class="space-y-4 md:space-y-6">
+                    <form className="space-y-4 md:space-y-6">
                       {/** Name Input */}
                       {
                         !values.isMember &&
@@ -47,6 +81,7 @@ const Register = () => {
                         name='name'
                         value={values.name} 
                         handleChange={handleChange}
+                        validateInput={validateInput}
                         labelText='Name' 
                         placeholder='your name'
                       />
@@ -59,6 +94,7 @@ const Register = () => {
                         name='email'
                         value={values.email} 
                         handleChange={handleChange}
+                        validateInput={validateInput}
                         labelText='Email' 
                         placeholder='name@company.com'
                       />
@@ -69,6 +105,7 @@ const Register = () => {
                         name='password'
                         value={values.password} 
                         handleChange={handleChange}
+                        validateInput={validateInput}
                         labelText='Password' 
                         placeholder='••••••••'
                       />
@@ -82,6 +119,7 @@ const Register = () => {
                         name='confirmPassword'
                         value={values.confirmPassword} 
                         handleChange={handleChange}
+                        validateInput={validateInput}
                         labelText='Confirm password' 
                         placeholder='••••••••'
                       />
@@ -91,24 +129,24 @@ const Register = () => {
                       {
                         !values.isMember &&
                       (
-                        <div class="flex items-start">
-                            <div class="flex items-center h-5">
-                              <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="" />
+                        <div className="flex items-start">
+                            <div className="flex items-center h-5">
+                              <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="" />
                             </div>
-                            <div class="ml-3 text-sm">
-                              <label for="terms" class="font-light text-gray-500">I accept the <a class="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
+                            <div className="ml-3 text-sm">
+                              <label for="terms" className="font-light text-gray-500">I accept the <a className="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
                             </div>
                         </div>
                         )
                         }
 
-                      <button class="w-full bg-[#2cb1bc] hover:bg-[#19838b] text-white font-bold py-2 px-4 mt-2 rounded-full">
+                      <button className="w-full bg-[#2cb1bc] hover:bg-[#19838b] text-white font-bold py-2 px-4 mt-2 rounded-full">
                         submit
                       </button>
-                      <p class="text-center text-sm font-light text-gray-500">
+                      <p className="text-center text-sm font-light text-gray-500">
                         {!values.isMember? 'Already a member? ' : 'Not a member yet? '} 
                           <button 
-                            className='font-medium text-primary-600 hover:underline'
+                            classNameName='font-medium text-primary-600 hover:underline'
                             onClick={toggleMember}
                             >
                             {!values.isMember? 'Login' : 'Register'}
